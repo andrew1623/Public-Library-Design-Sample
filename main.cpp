d7a5d4df44924ec6594bdffed83d2a387f8735b8
Andrew Thompson <AndrewThompson3@my.unt.edu>
2020-10-29 13:03:29 -0500

    Input w/ validation working

--- main.cpp
+++ main.cpp
@@ -1,37 +1,72 @@
 #include <iostream>
 #include <string>
 #include <cctype>
-#include "patrons/Patrons.h"
-#include "loans/Loans.h"
-#include "books/Books.h"
+// #include "patrons/Patrons.h"
+// #include "loans/Loans.h"
+// #include "books/Books.h"
 
 using namespace std;
 
 char getUserInput()
 {
-  char input;
+  string input;
   cout << "Enter a selection: " << endl;
   cin >> input;
-  // idk about this loop
-  // seems sus
-  if (cin.fail() || !isalpha(input))
+
+  //input validation
+  if (!isalpha(input[0]) || input.size() > 1)
   {
-    cout << "Selection must be a single character.\nEnter a selection: " << endl;
+    cout << "\nSelection must be a single character.\nEnter a selection: " << endl;
     cin >> input;
   }
   else
   {
-    return input;
+    return input[0];
   }
 }
 
-void menu();
+void menu()
+{
+  char currChoice;
+  while (currChoice != 'q')
+  {
+    cout << "\nMENU" << endl
+         << "(o) Check out a Book" << endl
+         << "(i) Check in a book" << endl
+         << "(b) Add a book to the Library" << endl
+         << "(p) Add a new patron" << endl
+         << "(f) Pay fines" << endl
+         << "(l) Report lost book" << endl
+         << "(q) Quit\n"
+         << endl;
+    currChoice = getUserInput();
+
+    switch (currChoice)
+    {
+    case 'o':
+      break;
+    case 'i':
+      break;
+    case 'b':
+      break;
+    case 'p':
+      break;
+    case 'f':
+      break;
+    case 'l':
+      break;
+    }
+  }
+}
 
 int main()
 {
-  Patrons patrons;
-  Loans loans;
-  Books books;
+
+  //   Patrons patrons;
+  //   Loans loans;
+  //   Books books;
+  // Need to load in books, loans, and patrons data file
+  menu();
 
   return 0;
 }
